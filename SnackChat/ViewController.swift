752dd14f5d7b3c67ff6c9b79289735b5cb7c4e08
Bob Pascazio <bob@bytefly.com>
2015-12-10 19:47:24 -0500

    updates

--- SnackChat/ViewController.swift
+++ SnackChat/ViewController.swift
@@ -10,16 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imagePreview: UIImageView!
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
+        
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func takeAPic(sender: AnyObject) {
+        
+        let image = UIImage(named: "Photo")
+        imagePreview.image = image
+    }
 
 }
 
